cfa2ff8238c79437c2b06f6d0d21b3a1174b9449
Deshar Sunil <deshario9@gmail.com>
2023-09-29 16:24:27 +0700

    Add keyword validation

--- apps/server/src/services/keyword/parseCsv.ts
+++ apps/server/src/services/keyword/parseCsv.ts
@@ -1,5 +1,8 @@
 export const parseCsv = (csvFile: Express.Multer.File) => {
   const contents = csvFile.buffer.toString('utf-8')
   const keywords = contents.split(/\r?\n/).filter(Boolean)
+  if (keywords.length > 100) {
+    throw new Error('The file should contain no more than 100 keyword')
+  }
   return keywords
 }
