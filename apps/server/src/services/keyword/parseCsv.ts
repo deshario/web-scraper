a068fd739ab1a151995fd9ed69b32fdc27bf7de5
Deshar Sunil <deshario9@gmail.com>
2023-10-02 15:24:45 +0700

    Move hardcoded value to constant

--- apps/server/src/services/keyword/parseCsv.ts
+++ apps/server/src/services/keyword/parseCsv.ts
@@ -1,7 +1,9 @@
+import { MAX_KEYWORDS } from '../../constants'
+
 export const parseCsv = (csvFile: Express.Multer.File) => {
   const contents = csvFile.buffer.toString('utf-8')
   const keywords = contents.split(/\r?\n/).filter(Boolean)
-  if (keywords.length > 100) {
+  if (keywords.length > MAX_KEYWORDS) {
     throw new Error('The file should contain no more than 100 keyword')
   }
   return keywords
